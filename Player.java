78ec58efd40fb6a175f4273ca033da8217a055db
Suvineet Singh <suvineet1998@gmail.com>
2018-06-06 01:15:20 +0100

    Add Player class

---
+++ Player.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Player {
+
+  Scanner scanner;
+  public Player(Scanner scanner) {
+    this.scanner = scanner;
+  }
+
+  public RockPaperScissors.Move makeMove() {
+    //TODO: Imlement this method
+    return null;
+  }
+}
